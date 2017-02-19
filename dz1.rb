29a0ee7f8565bd0f253084cd49c56ab10a911e26
BogomolSergo <breinkiller650@gmail.com>
2017-02-19 18:46:24 +0200

    =) there's something here, something Hindu

--- dz1.rb
+++ dz1.rb
@@ -1,54 +1,45 @@
 require 'benchmark'
+
 class String
-	public
-
-	def valid?()
-		iterations = 1000
-		Benchmark.bm do |bm|
-			bm.report do
-				iterations.times do
-					letters = self.scan(/\S/)
-					input_set = Hash.new
-					compare_param = 0
-					compare_set = {
-							'[' => ']',
-							'{' => '}',
-							'(' => ')',
-							'<' => '>'
-					}
-
-					if letters.size % 2 != 0
-						puts 'false'
-						exit
+public
+def valid?
+iterations = 1
+	Benchmark.bm do |bm|
+		bm.report do
+			iterations.times do
+				letters = self.scan(/\S/) # string to array of chars
+				input_set = Hash.new
+				compare_param = 0
+				compare_set = {           #hash for compare(as nominal) with input values
+					'[' => ']',             #relationship brackets
+					'{' => '}',
+					'(' => ')',
+					'<' => '>'
+				}
+				left_part = letters[0...letters.size / 2]                        # two arrays, 1:left path of...
+				right_part = letters[letters.size / 2..letters.size].reverse!    #input string, 2:rigth part
+				(0...left_part.size).each do |i|
+					(0...right_part.size).each do |j|                              #insert left & right part in hash...
+						break if j > i                                               # as keys & values
+						input_set[left_part[i]] = right_part[j]
 					end
-
-					left_part = letters[0...letters.size / 2]
-					right_part = letters[letters.size / 2 ..letters.size].reverse!
-
-					(0...left_part.size).each do |i|
-						(0...right_part.size).each do |j|
-							break if j > i
-							input_set[left_part[i]] = right_part[j]
+				end
+				input_set.each_pair do |a, b|                                   #compare nominal hash with input hash
+					compare_set.each_pair do |n, m|
+						if (a == n) && (b == m)
+							#puts "#{a} == #{b}"
+							compare_param += 1
 						end
 					end
-
-						input_set.each_pair do |a, b|
-							compare_set.each_pair do |n, m|
-								if (a == n) && (b == m)
-									#puts "#{a} == #{b}"
-									compare_param += 1
-								end
-							end
-						end
-					if compare_param != input_set.size
-						puts 'false'
-					else puts 'true'
-					end
+				end
+				if compare_param != input_set.size
+					puts 'false'
+				else puts 'true'
 				end
 			end
 		end
 	end
 end
-
-arr = '[{}]'
+end
+arr = '(<[{}]>)'
 arr.valid?
