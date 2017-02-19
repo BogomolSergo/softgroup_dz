9ccf0935633cfaaad243012689f1c182f35c454d
BogomolSergo <breinkiller650@gmail.com>
2017-02-19 16:24:19 +0200

    add benchmark

--- dz1.rb
+++ dz1.rb
@@ -1,43 +1,54 @@
+require 'benchmark'
 class String
 	public
 
 	def valid?()
-		letters = self.scan(/\S/)
-		input_set = Hash.new
-		compare_param = 0
-		compare_set = {
-				'[' => ']',
-				'{' => '}',
-				'(' => ')',
-				'<' => '>'
-		}
+		iterations = 1000
+		Benchmark.bm do |bm|
+			bm.report do
+				iterations.times do
+					letters = self.scan(/\S/)
+					input_set = Hash.new
+					compare_param = 0
+					compare_set = {
+							'[' => ']',
+							'{' => '}',
+							'(' => ')',
+							'<' => '>'
+					}
 
-		if letters.size % 2 != 0
-			puts 'false'
-			exit
-		end
+					if letters.size % 2 != 0
+						puts 'false'
+						exit
+					end
 
-		left_part = letters[0...letters.size / 2]
-		right_part = letters[letters.size / 2 ..letters.size].reverse!
+					left_part = letters[0...letters.size / 2]
+					right_part = letters[letters.size / 2 ..letters.size].reverse!
 
-		(0...left_part.size).each do |i|
-			(0...right_part.size).each do |j|
-				break if j > i
-				input_set[left_part[i]] = right_part[j]
-			end
-		end
+					(0...left_part.size).each do |i|
+						(0...right_part.size).each do |j|
+							break if j > i
+							input_set[left_part[i]] = right_part[j]
+						end
+					end
 
-			input_set.each_pair do |a, b|
-				compare_set.each_pair do |n, m|
-					if (a == n) && (b == m)
-						#puts "#{a} == #{b}"
-						compare_param += 1
+						input_set.each_pair do |a, b|
+							compare_set.each_pair do |n, m|
+								if (a == n) && (b == m)
+									#puts "#{a} == #{b}"
+									compare_param += 1
+								end
+							end
+						end
+					if compare_param != input_set.size
+						puts 'false'
+					else puts 'true'
 					end
 				end
 			end
-		if compare_param != input_set.size
-			puts 'false'
-		else puts 'true'
 		end
 	end
 end
+
+arr = '[{}]'
+arr.valid?
