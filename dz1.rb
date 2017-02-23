700870bfe47838600a5a12826f25a0f539460819
BogomolSergo <breinkiller650@gmail.com>
2017-02-23 21:34:13 +0200

    =) there's something here, something Hindu

--- dz1.rb
+++ dz1.rb
@@ -1,38 +1,36 @@
 require 'benchmark'
-
 class String
-public
 def valid?
-iterations = 1
-	Benchmark.bm do |bm|
-		bm.report do
-			iterations.times do
-				letters = self.scan(/\S/) # String to array of chars.
-				input_set = Hash.new
-				compare_param = 0
-				compare_set = {           # Hash for compare(as nominal) with input values.
-					'[' => ']',             # Relationship brackets.
-					'{' => '}',
-					'(' => ')',
-					'<' => '>'
-				}
-				left_part = letters[0...letters.size / 2]                        # Two arrays, 1:left path of
-				right_part = letters[letters.size / 2..letters.size].reverse!    #   input string, 2:rigth part.
-				(0...left_part.size).each do |i|
-					(0...right_part.size).each do |j|                              # Insert left & right part in hash
-						break if j > i                                               #   as keys & values.
-						input_set[left_part[i]] = right_part[j]
-					end
-				end
-				input_set.each_pair do |a, b|                                    # Compare nominal hash with input hash.
-					compare_set.each_pair do |n, m|
-						compare_param += 1 if (a == n) && (b == m)
-					end
-				end
-				puts compare_param == input_set.size ? 'true' : 'false'
-			end
-		end
-	end
+  iterations = 1
+  input_set = {}
+  compare_param = 0
+  compare_set = { # Hash for compare(as nominal) with input values.
+                  '[' => ']', # Relationship brackets.
+                  '{' => '}',
+                  '(' => ')',
+                  '<' => '>'
+  }
+  Benchmark.bm do |bm|
+    bm.report do
+      iterations.times do
+        line = scan(/\S/) # String to array of chars.
+        l_part = line[0...line.size / 2] # Two arrays, 1:left path of
+        r_part = line[line.size / 2..line.size].reverse! #   input string, 2: part.
+        (0...l_part.size).each do |i|
+          (0...r_part.size).each do |j| # Insert left & right part in hash
+            break if j > i #   as keys & values.
+            input_set[l_part[i]] = r_part[j]
+          end
+        end
+        input_set.each_pair do |a, b| # Compare nominal hash with input hash.
+          compare_set.each_pair do |n, m|
+            compare_param += 1 if (a == n) && (b == m)
+          end
+        end
+        puts compare_param == input_set.size ? 'true' : 'false'
+      end
+    end
+  end
 end
 end
 arr = '(<[{}]>)'
